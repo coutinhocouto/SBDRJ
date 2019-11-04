65fcd54a02f1b6d43ea675cc65257a3899e7bfa2
coutinhocouto <coutinhocouto@hotmail.com>
2019-11-04 12:56:51 -0300

    1.2.3

--- carrega_dados.js
+++ carrega_dados.js
@@ -1,7 +1,7 @@
 function carrega_dados () {
   document.addEventListener("deviceready", function() {
     var $deviceId = device.uuid || 1337;
-    $.post( "http://appsbdrj.kinghost.net/appsbdrj/recebepost.php", { form: "carrega_login", device: $deviceId })
+    $.post( "https://eventosdermatologicos.com.br/apps/sbdrj/recebepost.php", { form: "carrega_login", device: $deviceId })
       .done(function( data ) {
         if (data==='AUTORIZADO') window.location.href = "principal.html?logado";
         else $("#app").show();
