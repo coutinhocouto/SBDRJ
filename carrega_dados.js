677d3ff39ef6c3e63b0212136fcc1d25971a094e
coutinhocouto <coutinhocouto@hotmail.com>
2019-12-18 17:07:58 -0300

    1.2.5

--- carrega_dados.js
+++ carrega_dados.js
@@ -1,7 +1,7 @@
 function carrega_dados () {
   document.addEventListener("deviceready", function() {
     var $deviceId = device.uuid || 1337;
-    $.post( "https://eventosdermatologicos.com.br/apps/sbdrj/recebepost.php", { form: "carrega_login", device: $deviceId })
+    $.post( "https://app.visana.com.br/sbdrj/recebepost.php", { form: "carrega_login", device: $deviceId })
       .done(function( data ) {
         if (data==='AUTORIZADO') window.location.href = "principal.html?logado";
         else $("#app").show();
